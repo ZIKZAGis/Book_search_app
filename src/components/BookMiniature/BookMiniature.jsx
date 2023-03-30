99c96104f39b1f85f6ce3c321410193112f0fe00
Evgeniy <nikolaev_zheka-zik@bk.ru>
2023-03-30 15:20:42 +0300

    небольшое исправление стилизации

--- src/components/BookMiniature/BookMiniature.jsx
+++ src/components/BookMiniature/BookMiniature.jsx
@@ -2,20 +2,19 @@ import { Link } from "react-router-dom"
 import styles from './style.module.scss'
 
 const BookMiniature = ({book}, key) => {
-    return (
-      <div className={styles.miniature} key={key}>
-        <Link to={`/book/${book.id}`}>
-          <img
-            className={styles.img}
-            src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'https://i.pinimg.com/originals/aa/b0/dd/aab0dd7a13205ce05bff5ea2f2db866a.jpg'} 
-            alt={book.volumeInfo.title} width={150}
-          />
-          {book.volumeInfo.categories && <div className={styles.category}>{book.volumeInfo.categories[0]}</div>}
-          <h2 className={styles.title}>{book.volumeInfo.title}</h2>
-          {book.volumeInfo.authors && <p className={styles.author}>{book.volumeInfo.authors.join(', ')}</p>}
-        </Link>
-      </div>
-    )
+  return (
+    <Link to={`/book/${book.id}`} className={styles.miniature} key={key}>
+      <img
+        className={styles.img}
+        src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'https://i.pinimg.com/originals/aa/b0/dd/aab0dd7a13205ce05bff5ea2f2db866a.jpg'} 
+        alt={book.volumeInfo.title} width={150}
+      />
+      {book.volumeInfo.categories && <div className={styles.category}>{book.volumeInfo.categories[0]}</div>}
+      <h2 className={styles.title}>{book.volumeInfo.title}</h2>
+      {book.volumeInfo.authors && <p className={styles.author}>{book.volumeInfo.authors.join(', ')}</p>}
+    </Link>
+
+  )
 }
 
 export default BookMiniature
